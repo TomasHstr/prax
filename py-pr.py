643c58ee7bb245e7762655f01abfec9fdd3a3b47
Tomas Hestera <tomas.hestera06@gmail.com>
2023-05-26 15:54:51 +0200

    python - 5.den

--- py-pr.py
+++ py-pr.py
@@ -16,6 +16,29 @@
   #      print('*',end=' ')
    # print('')
 
+    #a = int(input("zadaj 1. cislo. . ."))
+    #b = int(input("zadaj 2. cislo. . ."))
+    #c = int(input("zadaj 3. cislo. . ."))
+    #V = ((a+b+c)/3)
+    #print(V)
+#List = []
+#maxLengthList = 30
+#a = int(input("zadaj cislo: "))
+#while 1 :
+ #   List.append(a)
+  #  a = int(input("zadaj cislo: "))
+   # print(List)
+    #if a == 0:
+     #   break
 
-a = int(input())
-print(a)
+
+
+ import random
+a = random.randrange(1,100)
+b = int(input("zadaj cislo"))
+if b > a:
+    print("mensie cislo.. .")
+if b < a:
+   print("vacsie cislo. . .")
+if b == a:
+   print("uhadol si!")
