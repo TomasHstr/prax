103be110f96540784adb50006d2ff4dab429257b
Tomas Hestera <tomas.hestera06@gmail.com>
2023-05-26 14:18:05 +0200

    geometrick

---
+++ py-pr.py
@@ -0,0 +1,21 @@
+#a = "*"
+#for i in range(5):
+ #   for j in range(5):
+  #      print('*',end=' ')
+   # print('')
+#print('')
+
+#for i in range(5):
+ #   for j in range(i+1):
+  #      print('*',end=' ')
+   # print('')
+#print('')
+
+#for i in reversed(range(5)):
+ #   for j in range(i+1):
+  #      print('*',end=' ')
+   # print('')
+
+
+a = int(input())
+print(a)
