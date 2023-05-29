baf2e7f928fe3567776e68209ff17ff9dd0d3138
Tomas Hestera <tomas.hestera06@gmail.com>
2023-05-29 12:34:51 +0200

    python

--- py-pr.py
+++ py-pr.py
@@ -22,17 +22,17 @@
     #V = ((a+b+c)/3)
     #print(V)
 
-List = []
-a = int(input("zadaj cislo: "))
-while 1 :
-    List.append(a)
-    a = int(input("zadaj cislo: "))
-    print(List)
-    b = sum(List)
-    c = len(List)
-    if a == 0:
-        print("priemer je. . .",b/c)
-        break
+#List = []
+#a = int(input("zadaj cislo: "))
+#while 1 :
+    #List.append(a)
+    #a = int(input("zadaj cislo: "))
+    #print(List)
+    #b = sum(List)
+    #c = len(List)
+    #if a == 0:
+        #print("priemer je. . .",1/(b/c)*100,"%")
+        #break
 
 
 
@@ -46,3 +46,14 @@ import random
         #print("Vacsie cislo...")
     #b = int(input("znova: "))
 #print("ok")
+
+
+#1. kontrl ci 1
+#2. ak je ukonci, inak:
+#3. cyklus for od 2 po cislo m
+#4. kontrl delitelnosti cisla cislom i ci je 0 //modulo, percento
+#5. if True nieje prvo else je
+
+b = int(input("zadaj celocislo. . . "))
+if b <= 1:
+    print("neni prvocislo ")
