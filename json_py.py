5a358beb3dd3c2fc686113a84e354b6926f88826
Tomas Hestera <tomas.hestera06@gmail.com>
2023-05-30 09:49:56 +0200

    json

--- json_py.py
+++ json_py.py
@@ -1,10 +1,22 @@
 import json
 json_file_path = "hello.json"
 
-# Open the JSON file and load its contents
 with open(json_file_path, "r") as file:
     json_data = json.load(file)
 print(json_data["employees"][2])
+#for i in json_data["employees"]:
+    #print(i["name"])
+    #print(i["department"])
+
+#for i in json_data["employees"]:
+    #if ["In Progress"] in ["projects"] :
+        #print([i"name"])
+    #print(i["department"])
+
+
+
 for i in json_data["employees"]:
-    print(i["name"])
-    print(i["department"])
+    for j in i["projects"]:
+        if j["status"] == "In Progress":
+            print(i["name"])
+            break
